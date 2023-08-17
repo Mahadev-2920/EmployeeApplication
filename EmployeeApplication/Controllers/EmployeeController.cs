29c2c8883c1053c795b9802fff54175ae45fef75
Mahadev Ayarekar <s.m.ayarekar@gmail.com>
2023-08-17 07:08:25 +0530

    Add project files.

---
+++ EmployeeApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace EmployeeApplication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        HttpClient client = new HttpClient();
+        public IActionResult Index()
+        {
+            List<Employee> Employee = new List<Employee>();
+            client.BaseAddress = new Uri("https://localhost:7078/api/Employee");
+            var response = client.GetAsync("Employee");
+            response.Wait();
+            var test = response.Result;
+
+            if (test.IsSuccessStatusCode)
+            {
+                string display = test.Content.ReadAsStringAsync().Result;
+                Employee = JsonConvert.DeserializeObject<List<Employee>>(display);
+            }
+            return View(Employee);
+        }
+    }
+}
---
+++ EmployeeApplication/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace EmployeeApplication.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+
+        public string EmpFname { get; set; } = null!;
+
+        public string EmpLname { get; set; } = null!;
+
+        public string EmpAddress { get; set; } = null!;
+
+        public string EmpEmailId { get; set; } = null!;
+
+        public int EmpSalary { get; set; }
+
+        public DateTime CreatedDt { get; set; }
+
+    }
+}
